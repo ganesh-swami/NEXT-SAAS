fb603bc841f05a14566e3c4fe7f2bdaddbcf4ff1
ganesh-swami <ganeshswami018@gmail.com>
2024-06-15 20:18:21 +0530

    logout redirect update

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/settings-tab.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/settings-tab.tsx
@@ -1,13 +1,13 @@
-'use client'
-import React, { ChangeEventHandler } from 'react'
+"use client";
+import React, { ChangeEventHandler } from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from '@/components/ui/accordion'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+} from "@/components/ui/accordion";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   AlignCenter,
   AlignHorizontalJustifyCenterIcon,
@@ -22,8 +22,8 @@ import {
   AlignVerticalJustifyStart,
   ChevronsLeftRightIcon,
   LucideImageDown,
-} from 'lucide-react'
-import { Tabs, TabsTrigger, TabsList } from '@/components/ui/tabs'
+} from "lucide-react";
+import { Tabs, TabsTrigger, TabsList } from "@/components/ui/tabs";
 import {
   Select,
   SelectContent,
@@ -32,24 +32,24 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
-import { useEditor } from '@/providers/editor/editor-provider'
-import { Slider } from '@/components/ui/slider'
+} from "@/components/ui/select";
+import { useEditor } from "@/providers/editor/editor-provider";
+import { Slider } from "@/components/ui/slider";
 
-type Props = {}
+type Props = {};
 
 const SettingsTab = (props: Props) => {
-  const { state, dispatch } = useEditor()
+  const { state, dispatch } = useEditor();
 
   const handleOnChanges = (e: any) => {
-    const styleSettings = e.target.id
-    let value = e.target.value
+    const styleSettings = e.target.id;
+    let value = e.target.value;
     const styleObject = {
       [styleSettings]: value,
-    }
+    };
 
     dispatch({
-      type: 'UPDATE_ELEMENT',
+      type: "UPDATE_ELEMENT",
       payload: {
         elementDetails: {
           ...state.editor.selectedElement,
@@ -59,18 +59,18 @@ const SettingsTab = (props: Props) => {
           },
         },
       },
-    })
-  }
+    });
+  };
 
   const handleChangeCustomValues = (e: any) => {
-    const settingProperty = e.target.id
-    let value = e.target.value
+    const settingProperty = e.target.id;
+    let value = e.target.value;
     const styleObject = {
       [settingProperty]: value,
-    }
+    };
 
     dispatch({
-      type: 'UPDATE_ELEMENT',
+      type: "UPDATE_ELEMENT",
       payload: {
         elementDetails: {
           ...state.editor.selectedElement,
@@ -80,22 +80,19 @@ const SettingsTab = (props: Props) => {
           },
         },
       },
-    })
-  }
+    });
+  };
 
   return (
     <Accordion
       type="multiple"
       className="w-full"
-      defaultValue={['Typography', 'Dimensions', 'Decorations', 'Flexbox']}
+      defaultValue={["Typography", "Dimensions", "Decorations", "Flexbox"]}
     >
-      <AccordionItem
-        value="Custom"
-        className="px-6 py-0  "
-      >
+      <AccordionItem value="Custom" className="px-6 py-0  ">
         <AccordionTrigger className="!no-underline">Custom</AccordionTrigger>
         <AccordionContent>
-          {state.editor.selectedElement.type === 'link' &&
+          {state.editor.selectedElement.type === "link" &&
             !Array.isArray(state.editor.selectedElement.content) && (
               <div className="flex flex-col gap-2">
                 <p className="text-muted-foreground">Link Path</p>
@@ -109,10 +106,7 @@ const SettingsTab = (props: Props) => {
             )}
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem
-        value="Typography"
-        className="px-6 py-0  border-y-[1px]"
-      >
+      <AccordionItem value="Typography" className="px-6 py-0  border-y-[1px]">
         <AccordionTrigger className="!no-underline">
           Typography
         </AccordionTrigger>
@@ -123,7 +117,7 @@ const SettingsTab = (props: Props) => {
               onValueChange={(e) =>
                 handleOnChanges({
                   target: {
-                    id: 'textAlign',
+                    id: "textAlign",
                     value: e,
                   },
                 })
@@ -181,7 +175,7 @@ const SettingsTab = (props: Props) => {
                 onValueChange={(e) =>
                   handleOnChanges({
                     target: {
-                      id: 'font-weight',
+                      id: "font-weight",
                       value: e,
                     },
                   })
@@ -212,10 +206,7 @@ const SettingsTab = (props: Props) => {
           </div>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem
-        value="Dimensions"
-        className=" px-6 py-0 "
-      >
+      <AccordionItem value="Dimensions" className=" px-6 py-0 ">
         <AccordionTrigger className="!no-underline">
           Dimensions
         </AccordionTrigger>
@@ -336,10 +327,7 @@ const SettingsTab = (props: Props) => {
           </div>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem
-        value="Decorations"
-        className="px-6 py-0 "
-      >
+      <AccordionItem value="Decorations" className="px-6 py-0 ">
         <AccordionTrigger className="!no-underline">
           Decorations
         </AccordionTrigger>
@@ -349,12 +337,12 @@ const SettingsTab = (props: Props) => {
             <div className="flex items-center justify-end">
               <small className="p-2">
                 {typeof state.editor.selectedElement.styles?.opacity ===
-                'number'
+                "number"
                   ? state.editor.selectedElement.styles?.opacity
                   : parseFloat(
                       (
-                        state.editor.selectedElement.styles?.opacity || '0'
-                      ).replace('%', '')
+                        state.editor.selectedElement.styles?.opacity || "0"
+                      ).replace("%", "")
                     ) || 0}
                 %
               </small>
@@ -363,18 +351,18 @@ const SettingsTab = (props: Props) => {
               onValueChange={(e) => {
                 handleOnChanges({
                   target: {
-                    id: 'opacity',
+                    id: "opacity",
                     value: `${e[0]}%`,
                   },
-                })
+                });
               }}
               defaultValue={[
-                typeof state.editor.selectedElement.styles?.opacity === 'number'
+                typeof state.editor.selectedElement.styles?.opacity === "number"
                   ? state.editor.selectedElement.styles?.opacity
                   : parseFloat(
                       (
-                        state.editor.selectedElement.styles?.opacity || '0'
-                      ).replace('%', '')
+                        state.editor.selectedElement.styles?.opacity || "0"
+                      ).replace("%", "")
                     ) || 0,
               ]}
               max={100}
@@ -386,12 +374,12 @@ const SettingsTab = (props: Props) => {
             <div className="flex items-center justify-end">
               <small className="">
                 {typeof state.editor.selectedElement.styles?.borderRadius ===
-                'number'
+                "number"
                   ? state.editor.selectedElement.styles?.borderRadius
                   : parseFloat(
                       (
-                        state.editor.selectedElement.styles?.borderRadius || '0'
-                      ).replace('px', '')
+                        state.editor.selectedElement.styles?.borderRadius || "0"
+                      ).replace("px", "")
                     ) || 0}
                 px
               </small>
@@ -400,19 +388,19 @@ const SettingsTab = (props: Props) => {
               onValueChange={(e) => {
                 handleOnChanges({
                   target: {
-                    id: 'borderRadius',
+                    id: "borderRadius",
                     value: `${e[0]}px`,
                   },
-                })
+                });
               }}
               defaultValue={[
                 typeof state.editor.selectedElement.styles?.borderRadius ===
-                'number'
+                "number"
                   ? state.editor.selectedElement.styles?.borderRadius
                   : parseFloat(
                       (
-                        state.editor.selectedElement.styles?.borderRadius || '0'
-                      ).replace('%', '')
+                        state.editor.selectedElement.styles?.borderRadius || "0"
+                      ).replace("%", "")
                     ) || 0,
               ]}
               max={100}
@@ -463,7 +451,7 @@ const SettingsTab = (props: Props) => {
               onValueChange={(e) =>
                 handleOnChanges({
                   target: {
-                    id: 'backgroundSize',
+                    id: "backgroundSize",
                     value: e,
                   },
                 })
@@ -494,10 +482,7 @@ const SettingsTab = (props: Props) => {
           </div>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem
-        value="Flexbox"
-        className="px-6 py-0  "
-      >
+      <AccordionItem value="Flexbox" className="px-6 py-0  ">
         <AccordionTrigger className="!no-underline">Flexbox</AccordionTrigger>
         <AccordionContent>
           <Label className="text-muted-foreground">Justify Content</Label>
@@ -505,7 +490,7 @@ const SettingsTab = (props: Props) => {
             onValueChange={(e) =>
               handleOnChanges({
                 target: {
-                  id: 'justifyContent',
+                  id: "justifyContent",
                   value: e,
                 },
               })
@@ -550,7 +535,7 @@ const SettingsTab = (props: Props) => {
             onValueChange={(e) =>
               handleOnChanges({
                 target: {
-                  id: 'alignItems',
+                  id: "alignItems",
                   value: e,
                 },
               })
@@ -581,10 +566,10 @@ const SettingsTab = (props: Props) => {
               onChange={(va) => {
                 handleOnChanges({
                   target: {
-                    id: 'display',
-                    value: va.target.checked ? 'flex' : 'block',
+                    id: "display",
+                    value: va.target.checked ? "flex" : "block",
                   },
-                })
+                });
               }}
             />
             <Label className="text-muted-foreground">Flex</Label>
@@ -601,7 +586,7 @@ const SettingsTab = (props: Props) => {
         </AccordionContent>
       </AccordionItem>
     </Accordion>
-  )
-}
+  );
+};
 
-export default SettingsTab
+export default SettingsTab;
--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,20 +1,20 @@
-import { ModeToggle } from '@/components/global/mode-toggle'
-import { UserButton } from '@clerk/nextjs'
-import { User } from '@clerk/nextjs/server'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { ModeToggle } from "@/components/global/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 type Props = {
-  user?: null | User
-}
+  user?: null | User;
+};
 
 const Navigation = ({ user }: Props) => {
   return (
     <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
       <aside className="flex items-center gap-2">
         <Image
-          src={'./assets/plura-logo.svg'}
+          src={"./assets/plura-logo.svg"}
           width={40}
           height={40}
           alt="plur logo"
@@ -23,24 +23,24 @@ const Navigation = ({ user }: Props) => {
       </aside>
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
-          <Link href={'#'}>Pricing</Link>
-          <Link href={'#'}>About</Link>
-          <Link href={'#'}>Documentation</Link>
-          <Link href={'#'}>Features</Link>
+          <Link href={"#"}>Pricing</Link>
+          <Link href={"#"}>About</Link>
+          <Link href={"#"}>Documentation</Link>
+          <Link href={"#"}>Features</Link>
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
         <Link
-          href={'/agency'}
+          href={"/agency"}
           className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
         >
           Login
         </Link>
-        <UserButton />
+        <UserButton afterSignOutUrl={process.env.NEXT_PUBLIC_URL} />
         <ModeToggle />
       </aside>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
