169a35f1eddb5f4e2dc952dfc63d9040bdef0a59
ganesh-swami <ganeshswami018@gmail.com>
2024-06-17 22:18:12 +0530

    breadcrumb updated

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -24,7 +24,7 @@ const Navigation = ({ user }: Props) => {
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
           <Link href={"#"}>Pricing</Link>
-          <Link href={"#"}>About</Link>
+          <Link href={"#"}>About US</Link>
           <Link href={"#"}>Documentation</Link>
           <Link href={"#"}>Features</Link>
         </ul>
